842600e90f3cb04d98bfcf2c32de0b3bd65146e1
Shamshad Hussain <shamshad3300@gmail.com>
2022-02-08 22:29:43 +0530

    format code

--- src/App.js
+++ src/App.js
@@ -1,37 +1,33 @@
-import './App.css';
-import Layout from './layouts/index';
-import Login from './pages/Login';
-import Dashboard from './pages/Home/components/Dashboard';
-import Mrf from './pages/Home/components/Mrf';
+import "./App.css";
+import Layout from "./layouts/index";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Home/components/Dashboard";
+import Mrf from "./pages/Home/components/Mrf";
 
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   const user = false;
   return (
-   <>
-    {user ? (
-      <Layout> 
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Dashboard/>} />
-      <Route path='mrf' element={<Mrf/>}/>
-    </Routes>
-  </BrowserRouter>,
-  </Layout> 
-    ) : (
-      <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Login/>} />
-      </Routes>
-  </BrowserRouter>
-    )}
-   
-   </>
+    <>
+      {user ? (
+        <Layout>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="mrf" element={<Mrf />} />
+            </Routes>
+          </BrowserRouter>
+          ,
+        </Layout>
+      ) : (
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      )}
+    </>
   );
 }
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,57 +1,74 @@
-import './Login.css'
-import React from 'react';
+import "./Login.css";
+import React from "react";
 import { Link } from "react-router-dom";
 
 const Login = () => {
-  return(
-      <div className='container-fluid main-bg'>
-            <div className='container'>
-                <div className='row'>
-                    <div className='col-lg-8'></div>
-                    <div className='col-lg-4'>
-                        <div className='form-container'>
-                            <div className='form-wrapper'>
-                                <div className='logo-container'>
-                                    <h1>NEXT</h1>
-                                </div>
-                                    <div className='input-fields'>
-                                    <input type="email" name="" id="" placeholder='Email / Emp ID' autoComplete="new-password" required />
-                                    <input type="password" name="" id="" placeholder='Password'  autoComplete="new-password" required/>
-                                    </div>
-
-                                    <div className='remember-toggle'>
-                                    <label class="switch">
-                                     <input type="checkbox" name=''  />
-                                    <span class="slider round"></span>
-                                    </label>
-                                        <h5>Remember Me</h5>
-                                    </div>
-
-                                    <div className='signIn-container'>
-                                    <Link to="/forget"><h6>Forget Password</h6></Link>
-                                        <button> SIGN IN</button>
-                                    </div>
-
-                                    <div className='powered-by-container'>
-                                        <p>Powered By</p>
-                                        <h6>NEXT</h6>
-                                    </div>
-                                    <div className='copy-right'>
-                                        <p>&copy; 2022 Next Digital Pvt Ltd. </p>
-                                    </div>
-
-                                    <div className='check-term-privacy-policy'>
-                                        <p>Cookie Policy</p>
-                                        <p>Term Of Use</p>
-                                        <p>Privacy Policy</p>
-                                    </div>
-                            </div>
-                        </div>
-                    </div>
+  return (
+    <div className="container-fluid main-bg">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-8"></div>
+          <div className="col-lg-4">
+            <div className="form-container">
+              <div className="form-wrapper">
+                <div className="logo-container">
+                  <h1>NEXT</h1>
                 </div>
+                <div className="input-fields">
+                  <input
+                    type="email"
+                    name=""
+                    id=""
+                    placeholder="Email / Emp ID"
+                    autoComplete="new-password"
+                    required
+                  />
+                  <input
+                    type="password"
+                    name=""
+                    id=""
+                    placeholder="Password"
+                    autoComplete="new-password"
+                    required
+                  />
+                  ̦
+                </div>
+
+                <div className="remember-toggle">
+                  <label class="switch">
+                    <input type="checkbox" name="" />
+                    <span class="slider round"></span>
+                  </label>
+                  <h5>Remember Me</h5>
+                </div>
+
+                <div className="signIn-container">
+                  <Link to="/forget">
+                    <h6>Forget Password</h6>
+                  </Link>
+                  <button> SIGN IN</button>
+                </div>
+
+                <div className="powered-by-container">
+                  <p>Powered By</p>
+                  <h6>NEXT</h6>
+                </div>
+                <div className="copy-right">
+                  <p>&copy; 2022 Next Digital Pvt Ltd. </p>
+                </div>
+
+                <div className="check-term-privacy-policy">
+                  <p>Cookie Policy</p>
+                  <p>Term Of Use</p>
+                  <p>Privacy Policy</p>
+                </div>
+              </div>
             </div>
+          </div>
+        </div>
       </div>
-    );
+    </div>
+  );
 };
- 
+
 export default Login;
